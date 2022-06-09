5fedc973f56237a06d7f8c824ef29a35d7a019ea
alexfoster1022 <alexander.f.holman@gmail.com>
2022-06-09 15:45:33 -0500

    added navbar to UI - including logo and avatar. Added simple animations and transitions.

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import "./App.css";
 import Row from "./components/Row";
 import requests from "./requests";
 import Banner from "./components/Banner";
+import Navbar from "./components/Navbar";
 
 const App = () => {
   return (
     <div className="App">
+      <Navbar />
       <Banner />
       <Row
         title="NETFLIX ORIGINALS"
